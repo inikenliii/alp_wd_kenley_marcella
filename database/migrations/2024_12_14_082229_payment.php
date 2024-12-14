b4cb5eb06f60034997bf26c9331e6dacd58f52bb
inikenliii <153164728+inikenliii@users.noreply.github.com>
2024-12-14 15:39:50 +0700

    aowkoakokwaokw

---
+++ database/migrations/2024_12_14_082229_payment.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained( 
+                table: 'users',
+                indexName: 'payments_user_id' 
+            );
+            $table->date('payment_date');
+            $table->decimal('amount');
+            $table->date('month_paid');
+            $table->enum('payment_status',['paid', 'pending']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
