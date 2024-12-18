59bdeb4274cfea9f4bd48892f9ea68fe60d52449
Marcellas87 <meonggarong86862@outlook.com>
2024-12-18 16:59:34 +0700

    my push

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <x-navbar></x-navbar>
+    <h1 class="text-5xl font-bold">Home</h1>
+</body>
+</html>
