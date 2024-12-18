30d769f1cbb1119bc14cddb5ce2b1546c1de0da0
Marcellas87 <meonggarong86862@outlook.com>
2024-12-18 21:43:22 +0700

    push/

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -33,7 +33,7 @@
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="/" class="{{ request()-> is ('/') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white border-2 border-orange-700 hover:border-orange-400'}}" aria-current="page">Home</a>
             <a href="/session" class="{{ request()-> is ('/session') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white'}}" aria-current="page">Session</a>
-            <a href="/attendance" class="{{ request()-> is ('/attendance') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white'}}" aria-current="page">Payment</a>
+            <a href="/attendance" class="{{ request()-> is ('/attendance') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white'}}" aria-current="page">Attendance</a>
             <a href="/payment" class="{{ request()-> is ('/payment') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white'}}" aria-current="page">Payment</a>
           </div>
         </div>
@@ -41,6 +41,11 @@
 
       <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
 
+        <a href="/profile" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+          <span class="absolute -inset-1.5"></span>
+          <span class="sr-only">Open user menu</span>
+          <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+        </a>
       </div>
 
     </div>
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+
+    <x-slot:headerTitle>{{$pagetitle}}</x-slot:headerTitle>
+    <x-slot:bgColor>{{ 'bg-orange-950' }}</x-slot:bgColor>
+
+    <div class="flex">
+        
+    </div>
+
+</x-layout>
