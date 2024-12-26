a5cacb18efbed5b3f7bace8b3a3b21ce6050a565
Marcellas87 <meonggarong86862@outlook.com>
2024-12-26 08:40:09 +0700

    remove dbshow

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -35,7 +35,6 @@
             <a href="/session/{{ $slot }}" class="{{ request()-> is ('/sessionb') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white'}}" aria-current="page">Session</a>
             <a href="/attendance/{{ $slot }}" class="{{ request()-> is ('/attendanceb') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white'}}" aria-current="page">Attendance</a>
             <a href="/payment/{{ $slot }}" class="{{ request()-> is ('/paymentb') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white'}}" aria-current="page">Payment</a>
-            <a href="/dbshow/{{ $slot }}" class="{{ request()-> is ('/paymentb') ? 'hidden' : 'rounded-md px-3 py-2 text-sm font-medium text-yellow-400 bg-orange-950 hover:bg-amber-700 hover:text-white'}}" aria-current="page">Database Show</a>
           </div>
         </div>
       </div>
